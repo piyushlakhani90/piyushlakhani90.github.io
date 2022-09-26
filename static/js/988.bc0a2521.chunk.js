78c7dd1b2e5462d5e85e572ac3aa606b9779ede5
piyush-1309 <114130536+piyush-1309@users.noreply.github.com>
2022-09-26 17:22:43 +0530

    Updates

---
+++ static/js/988.bc0a2521.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkciara_donnen=self.webpackChunkciara_donnen||[]).push([[988],{8988:function(n,e,r){r.r(e);var a=r(5671),t=r(3144),u=r(136),i=r(9388),c=r(2791),o=r(184),s=function(n){(0,u.Z)(r,n);var e=(0,i.Z)(r);function r(){return(0,a.Z)(this,r),e.apply(this,arguments)}return(0,t.Z)(r,[{key:"render",value:function(){return(0,o.jsx)("div",{children:"dashboard"})}}]),r}(c.Component);e.default=s}}]);
+//# sourceMappingURL=988.bc0a2521.chunk.js.map
